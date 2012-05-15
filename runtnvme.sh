5e9ba0ee518267957dc4c841f669653ae7530556
Sravan Kumar Thokala <sravan.kumar.thokala@intel.com>
2012-05-15 10:58:18 -0600

    Updated runtnvme script to wait for svlogd status
        The runtnvme scirpt is modified to get the status for the
        svlogd logging. Also reduced the stdout logging to 10 lines.

--- runtnvme.sh
+++ runtnvme.sh
@@ -18,7 +18,6 @@
 starttime="$(date +%s)"
 TNVME_CMD_LINE=$@
 BASE_OUT_DIR=./Logs
-RUNNING_TEST=false
 
 Usage() {
 echo "usage...."
@@ -29,10 +28,6 @@ echo ""
 if [ -z "$TNVME_CMD_LINE" ]; then
   Usage
   exit -1
-fi
-
-if [[ "$TNVME_CMD_LINE" == *-t* ]]; then
-    RUNNING_TEST=true
 fi
 
 # Create a root logging directory. Sub-directories will be created by tnvme
@@ -50,26 +45,14 @@ echo "n3" >>${BASE_OUT_DIR}/config
 # ./Logs/GrpPending contains the resource dumps of the last group which executed
 # ./Logs/current is the current output from tnvme via stderr/stdout
 # ./Logs/*.s files are the result of svlogd rotating ./${BASE_OUT_DIR}/current
-if [ $RUNNING_TEST == true ]; then
-    # Pipe tnvme into the logging utility for 8 fold speed increase
-    ../tnvme/tnvme --dump=${BASE_OUT_DIR} -k skiptest.cfg --golden=./identify.gold.xml $TNVME_CMD_LINE 2>&1 | svlogd -v -tt -b 2048 -l 0 ${BASE_OUT_DIR}
-    ret=${PIPESTATUS[0]}
-else
-    # Allow tnvme to be slow, because we want to see the output immediately
-    ../tnvme/tnvme --dump=${BASE_OUT_DIR} $TNVME_CMD_LINE 2>&1 
-    ret=${PIPESTATUS[0]}
-fi
+../tnvme/tnvme --dump=${BASE_OUT_DIR} -k skiptest.cfg --golden=./identify.gold.xml $TNVME_CMD_LINE 2>&1 | svlogd -v -tt -b 2048 -l 0 ${BASE_OUT_DIR}
+ret=${PIPESTATUS[0]}
+logger_ended=${PIPESTATUS[1]}
+tail --lines=10 ${BASE_OUT_DIR}/current
 
 # Cleanup files used to rotate logs, they are just noise
 rm -f ${BASE_OUT_DIR}/lock
 rm -f ${BASE_OUT_DIR}/config
-
-# Report the end of the current log file
-if [[ $ret -ne 0 ]]; then
-    tail --lines=50 ${BASE_OUT_DIR}/current | grep -C 4 "Iteration SUMMARY"
-else
-    tail --lines=50 ${BASE_OUT_DIR}/current | grep -A 4 "Iteration SUMMARY"
-fi
 
 stoptime="$(date +%s)"
 delta="$(expr $stoptime - $starttime)"
